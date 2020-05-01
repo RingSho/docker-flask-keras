b050f708edd06a1d7958cefc2ca7511ad5ecafe0
Apollo <hayashi-shohei@signpost.co.jp>
2020-05-01 16:03:19 +0900

    Fix: some code

--- README.md
+++ README.md
@@ -1,8 +1,35 @@
-# Docker
+# 機械学習マイクロサービス(Reastful-API, keras, tensorflow) on GPU Docker
+
+## 開発環境
+
+Ubuntu 18.04  
+Docker 19.03.8  
+nvidia-container-runtime 1.0.0-rc10  
+
+## 前提
+
+nvidia-smiがローカルで動くこと  
+pythonでgpuを使った学習が行えていること  
+
+## Docker
+
+<https://docs.docker.com/engine/install/ubuntu/>
+
+## Nvidia-container-runtime
+
+<https://github.com/NVIDIA/nvidia-container-runtime>
 
 ## no cache build
 
 `docker-compose build --no-cache`
+
+## apt-get updateでハッシュ不適合のエラーが出たら
+
+ホスト側で以下を実行  
+
+`sudo rm -rf /var/lib/apt/lists/*`  
+`sudo rm -rf /var/lib/apt/lists`  
+`sudo apt-get update`
 
 ## コンテナ全stop
 
@@ -15,8 +42,3 @@
 ## イメージ全削除
 
 `docker images -aq | xargs docker rmi`
-
-## apt-get updateでハッシュ不適合のエラーが出たら
-
-`sudo rm -rf /var/lib/apt/lists/*`
-`sudo apt-get update`
--- my_mnist.py
+++ my_mnist.py
@@ -1,6 +1,7 @@
 import numpy as np
 import struct
 from PIL import Image
+import os
  
 def output_mnist_img_png( source_mnist_img_file ):
     f = open(source_mnist_img_file, 'rb')
@@ -10,6 +11,7 @@ def output_mnist_img_png( source_mnist_img_file ):
     number_of_columns = struct.unpack('>i', f.read(4))[0]
     bytes_per_image = number_of_rows * number_of_columns
     format = '%dB' % bytes_per_image
+    os.makedirs("mnist_images", exist_ok=True)
     for i in range(0, number_of_images):
         raw_img = f.read(bytes_per_image)
         lin_img = struct.unpack(format, raw_img)
@@ -17,4 +19,4 @@ def output_mnist_img_png( source_mnist_img_file ):
         np_ary  = np.reshape(np_ary, (28,28),order='C')
         pil_img = Image.fromarray(np_ary)
         fpath   = '%05d.png' % i
-        pil_img.save(fpath)
+        pil_img.save("mnist_images/" + fpath)
